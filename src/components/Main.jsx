fea037dd1236a4c69b15188dad63fe009456ab06
Felix-Okeyo <felixokeyo20@gmail.com>
2023-10-11 17:25:52 +0300

    main page work ongoing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,6 +6,8 @@ import requests from '../Requests'
 const Main = () => {
 
   const [movies, setMovies] = useState([])
+
+  const movie = movies[Math.floor(Math.random() * movies.length)]
   
   useEffect(() => {
     axios.get(requests.requestPopular)
@@ -15,12 +17,30 @@ const Main = () => {
 
 
   }, [])
-  console.log(movies)
+  console.log(movie)
 
 
   
   return (
-    <div>main</div>
+    <div className='w-full h-[550px] text-white'>
+      <div className ='w-full h-full'>
+      <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+      <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}/>
+      </div>
+      <div className = 'absolute w-full top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+        <div className='my-4'>
+          <div>
+              <button className='border bg-gray-300 text-black py-2 px-5'>Play</button>
+              <button className='border text-white py-2 px-5 ml-4'>Watch Later</button>
+
+          </div>
+        </div>
+        
+      </div>
+      
+
+    </div>
   )
 }
 
