8f69b5000e9844247255bb526b75a85aab7bab7d
Felix-Okeyo <felixokeyo20@gmail.com>
2023-10-21 13:59:52 +0300

    auth by firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import {Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 
 function App() {
   return (
     <>
-    <Navbar />
+    <AuthContextProvider>
+      <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
-
-    </Routes>
+    </Routes> 
+    </AuthContextProvider>
+   
     </>
   );
 }
