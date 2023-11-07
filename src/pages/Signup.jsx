4a366e4909383fb08066f3abbad1e002b60bd501
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-07 15:47:24 +0300

    sign in styling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Signup = () => {
+
+function Signup (){
+
+    
   return (
     <>
         <div className='w-full h-screen'>
